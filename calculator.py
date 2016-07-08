dc2c84aaaea0029c4e5df8ffb92fd902174dc049
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 17:24:42 -0700

    Updated variable names.

--- calculator.py
+++ calculator.py
@@ -14,35 +14,35 @@ def calculator():
         entered_string = raw_input(">> ")
         user_input = entered_string.split(" ")
         
-        u0 = user_input[0]
-        u1 = int(user_input[1])
+        operation = user_input[0]
+        operand1 = int(user_input[1])
 
         if len(user_input) == 3:
-            u2 = int(user_input[2])
-        #u0, u1, u2 = user_input
+            operand2 = int(user_input[2])
+        #operation, operand1, operand2 = user_input
 
         result = None
 
-        # if u0.lower() == "q":
+        # if operation.lower() == "q":
         #     return 
-        if u0 == "+":
-            result = add(u1, u2)
-        elif u0 == "-":
-            result = subtract(u1, u2)
-        elif u0 == "*":
-            result = multiply(u1, u2)
-        elif u0 == "/":
-            result = divide(u1, u2)
-        elif u0 == "square":
-            result = square(u1)
-        elif u0 == "cube":
-            result = cube(u1)
-        elif u0 == "power":
-            result = power(u1, u2)
-        elif u0 == "mod":
-            result = mod(u1, u2)
+        if operation == "+":
+            result = add(operand1, operand2)
+        elif operation == "-":
+            result = subtract(operand1, operand2)
+        elif operation == "*":
+            result = multiply(operand1, operand2)
+        elif operation == "/":
+            result = divide(operand1, operand2)
+        elif operation == "square":
+            result = square(operand1)
+        elif operation == "cube":
+            result = cube(operand1)
+        elif operation == "power":
+            result = power(operand1, operand2)
+        elif operation == "mod":
+            result = mod(operand1, operand2)
         else:
-            entered_string = raw_input("I don't understand. Please try again.\n> ")
+            entered_string = raw_input("I don't understand. Please try again.\n>> ")
 
         print result
     return
